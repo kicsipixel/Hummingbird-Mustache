4221c944f833995952757dd914cb3705abe4a88f
Szabolcs Tóth <tsz@purzelbaum.hu>
2024-08-31 16:13:54 +0200

    Show was added.

--- ParksOfPrague/Sources/App/Controllers/WebsitesController.swift
+++ ParksOfPrague/Sources/App/Controllers/WebsitesController.swift
@@ -19,15 +19,56 @@ struct WebsitesController {
     let mustacheLibrary: MustacheLibrary
     
     func addRoutes(to router: Router<some RequestContext>) {
-        router.get("/", use: self.indexHandler)
+        router.get("/", use: self.index)
+        router.get("/park/:id", use: self.show)
     }
     
-    @Sendable func indexHandler(request: Request, context: some RequestContext) async throws -> HTML {
+    @Sendable func index(request: Request, context: some RequestContext) async throws -> HTML {
         let parks = try await Park.query(on: self.fluent.db()).all()
         
-        let context = IndexContext(parks: parks)
+        let parkContext = parks.map { park in
+            ParkContext(id: park.id,
+                                     name: park.name,
+                                     coordinates: ParkContext.Coordinates(latitude: park.coordinates.latitude,
+                                                                                       longitude: park.coordinates.longitude))
+        }
+        
+        let context = IndexContext(parkContexts: parkContext)
         
         guard let html = self.mustacheLibrary.render(context, withTemplate: "index") else {
+            throw HTTPError(.internalServerError, message: "Failed to render template.")
+        }
+        return HTML(html: html)
+    }
+    
+    @Sendable func show(request: Request, context: some RequestContext) async throws -> HTML {
+        let id = try context.parameters.require("id", as: UUID.self)
+        guard let park = try await Park.find(id, on: self.fluent.db()) else {
+            throw HTTPError(.notFound, message: "Park was not found")
+        }
+        
+        let parkContext = ParkContext(id: park.id,
+                                      name: park.name,
+                                      coordinates: ParkContext.Coordinates(latitude: park.coordinates.latitude,
+                                                                           longitude: park.coordinates.longitude))
+        
+        let context = ShowContext(parkContext: parkContext)
+        
+        guard let html = self.mustacheLibrary.render(context, withTemplate: "show") else {
+            throw HTTPError(.internalServerError, message: "Failed to render template.")
+        }
+        return HTML(html: html)
+    }
+    
+    @Sendable func test(request: Request, context: some RequestContext) async throws -> HTML {
+        let id = try context.parameters.require("id", as: UUID.self)
+        guard let park = try await Park.find(id, on: self.fluent.db()) else {
+            throw HTTPError(.notFound, message: "Park was not found")
+        }
+        
+        let context = TestContext(park: park)
+        
+        guard let html = self.mustacheLibrary.render(context, withTemplate: "test") else {
             throw HTTPError(.internalServerError, message: "Failed to render template.")
         }
         return HTML(html: html)
@@ -36,5 +77,24 @@ struct WebsitesController {
 
 /// Contexts
 struct IndexContext: Codable {
-    let parks: [Park]
+    let parkContexts: [ParkContext]
+}
+
+struct ShowContext: Codable {
+    let parkContext: ParkContext
+}
+
+struct ParkContext: Codable {
+    let id: UUID?
+    let name: String
+    let coordinates: Coordinates
+    
+    struct Coordinates: Codable {
+        let latitude: Double
+        let longitude: Double
+    }
+}
+
+struct TestContext: Codable {
+    let park: Park
 }
